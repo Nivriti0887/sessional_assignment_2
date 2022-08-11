feefec1512cef8a559564a9adc398f83adbfe408
Nivriti <nivriti0887.be20@chitkara.edu.in>
2022-08-11 19:40:25 +0530

    added nav bar

---
+++ scripts.js
@@ -0,0 +1,28 @@
+const xhr = new XMLHttpRequest();
+
+const url = 'https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09';
+
+xhr.open('GET',url,false);
+
+xhr.onreadystatechange = () => {
+    if (xhr.status === 200 && xhr.readyState === 4) {
+        const response = JSON.parse(xhr.responseText)
+        // console.log(response);
+
+        let template = '';
+        for(let i=0; i<response.length; i++){
+            template += `
+            <div class="tempo" style="width: 18rem;">
+                <div class="tempo-body">
+                    <h3 class="tempo-title">${response[i].name}</h3>
+                    <h5 class="tempo-title">${response[i].username}</h5>
+                    <h5 class="tempo-title">${response[i].email}</h5>
+                    <h5 class="tempo-title">${response[i].phone}</h5>
+                    <h5 class="tempo-title">${response[i].website}</h5>
+                </div>
+            </div>`;
+        }
+        document.querySelector('#my-tempo').innerHTML = template
+    }
+}
+xhr.send();
